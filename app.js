4b8bffea5162e132368be622ea6be16272378d16
Toure Lassana <l.toure910@laposte.net>
2024-10-25 15:53:32 +0200

    base de donnees prete

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const cors = require('cors');
-app.use(cors());
 
 var app = express();
+
+app.use(cors());
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
