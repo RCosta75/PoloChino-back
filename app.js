2c36b91acb6a7e5059bf45bc0335743b8f4e174c
Your name <Your email>
2024-11-07 18:32:43 +0100

    raph vercel

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -14,13 +13,14 @@ var ordersRouter = require("./routes/orders");
 var reviewsRouter = require("./routes/reviews");
 
 var app = express();
-
+const cors = require("cors");
 app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
