ac6e3e322c57fb1a7ac2624f7191250171e7ecda
Your name <Your email>
2024-11-07 18:28:03 +0100

    raph vercel

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-require('dotenv').config();
-require('./models/connection')
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
+require("dotenv").config();
+require("./models/connection");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var likesRouter = require('./routes/likes');
-var polosRouter = require('./routes/polos');
-var ordersRouter = require('./routes/orders');
-var reviewsRouter = require('./routes/reviews');
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var likesRouter = require("./routes/likes");
+var polosRouter = require("./routes/polos");
+var ordersRouter = require("./routes/orders");
+var reviewsRouter = require("./routes/reviews");
 
 var app = express();
 
 app.use(cors());
 
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/likes', likesRouter);
-app.use('/polos', polosRouter);
-app.use('/orders', ordersRouter);
-app.use('/reviews', reviewsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/likes", likesRouter);
+app.use("/polos", polosRouter);
+app.use("/orders", ordersRouter);
+app.use("/reviews", reviewsRouter);
 
 module.exports = app;
